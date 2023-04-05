261bf75fc1acc370ed32b9ef3026420970161adf
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-04-06 00:27:54 +0530

    added JS validation of password and ui improvement

---
+++ script.js
@@ -0,0 +1,24 @@
+const password = document.querySelector('#user-pass');
+const confirmPassword = document.querySelector('#confirm-pass');
+const errMsg = document.querySelector('.error-message');
+const form = document.querySelector('form');
+
+const submit = (e) => {
+    e.preventDefault();
+    if (password.value !== confirmPassword.value) {
+        password.classList.remove('success');
+        confirmPassword.classList.remove('success');
+        password.classList.add('error');
+        confirmPassword.classList.add('error');
+        errMsg.textContent = "*Passwords don't match";
+    }
+    else{
+        password.classList.remove('error');
+        confirmPassword.classList.remove('error');
+        errMsg.textContent = '';
+        password.classList.add('success');
+        confirmPassword.classList.add('success');
+    }
+}
+
+form.addEventListener('submit', submit);
